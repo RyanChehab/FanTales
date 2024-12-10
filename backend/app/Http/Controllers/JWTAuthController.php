ec93e00c3d6c43fabeefd11c2f4860d67dd76c74
RyanChehab <ryanchehab20@gmail.com>
2024-12-10 15:10:04 +0200

    php <=> created login function

--- backend/app/Http/Controllers/JWTAuthController.php
+++ backend/app/Http/Controllers/JWTAuthController.php
@@ -9,7 +9,18 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
-class JWTAuthController extends Controller
-{
-    //
+class JWTAuthController extends Controller{
+    
+    public function login(Request $request){
+    //Validating the request
+    $credentials = $request->only('email', 'password');
+    $validator = Validator::make($credentials, [
+        'email' => 'required|email',
+        'password' => 'required|min:6',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()], 422);
+    }
+    }                                   
 }
