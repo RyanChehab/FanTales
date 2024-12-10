c0efa3fba1e1b2e47b3859a4fcc6ab9cb871550b
RyanChehab <ryanchehab20@gmail.com>
2024-12-10 14:58:19 +0200

    php <=> used Request Hash Validator and Jwtexception

--- backend/app/Http/Controllers/JWTAuthController.php
+++ backend/app/Http/Controllers/JWTAuthController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class JWTAuthController extends Controller
 {
