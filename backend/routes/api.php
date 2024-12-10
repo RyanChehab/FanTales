c44dad4ca785c22704157f9b38d1594f100c9d51
RyanChehab <ryanchehab20@gmail.com>
2024-12-10 20:41:41 +0200

    php <=> Routed singup api

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTAuthController;
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/signup',[JWTAuthController::class, 'signup']);
